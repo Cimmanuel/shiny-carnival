7297f8460440cf424f6ba79f16bd5507bea4c2f3
Immanuel Kolapo <immanuelcaspian@gmail.com>
2019-03-18 02:41:39 +0100

    Schema for links and users app

---
+++ links/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Link(models.Model):
+    url = models.URLField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.url
